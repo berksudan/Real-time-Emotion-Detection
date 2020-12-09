3d4669b91d3415078809433345d9a608fac1cef3
Berk Sudan <berk.sudan@protonmail.com>
2020-12-09 21:42:18 +0300

    run and build scripts are modified for better setup

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Change current directory to project directory. 
-CURRENT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $CURRENT_PATH 
+CURRENT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+cd "$CURRENT_PATH" || exit
+
+# Install python3-venv package if not installed.
+sudo apt install python3-venv
 
 # Create virtual environment directory
 python3 -m venv venv/
@@ -12,12 +15,13 @@ source venv/bin/activate
 
 # Upgrade Python 
 python -m pip install --upgrade pip
-python -m pip3 install --upgrade pip
 
 # Check version of pip
 # Version must be below 18.XX and compatible with Python 3.4+
 pip --version
 
 # Install dependencies
-pip install -I opencv-python numpy pandas sklearn
-pip install -I cmake dlib
+pip install -I opencv-python==4.4.0.46
+pip install -I dlib==19.21.1
+pip install -I pandas==1.1.4
+pip install -I scikit-learn==0.23.2
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ CURRENT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 PYTHON_BIN=$CURRENT_PATH/venv/bin/python3
 PYTHON_MAIN=$CURRENT_PATH/main.py
 
-cd $CURRENT_PATH # Change directory to where bash script resides. 
-$PYTHON_BIN $PYTHON_MAIN
+cd "$CURRENT_PATH" # Change directory to where bash script resides. 
+"$PYTHON_BIN" "$PYTHON_MAIN"
